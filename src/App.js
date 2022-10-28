d5a5b76bf2581b0e8fd7f6c78fa66487f42003c2
Moscow <afker2020@gmail.com>
2022-10-28 01:17:04 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <main>
+      <div className="container w-[375px] mx-auto font-poppins">
+        <h1 className=" pt-20 font-[200] text-3xl text-GrayishBlue leading-10  ">
+          Reliable,efficient delivery{" "}
+          <span className=" font-[600] text-VeryDarkBlue   ">
+            {" "}
+            Powered by technology
+          </span>{" "}
+        </h1>
+        <p className=" text-[18px] mt-4 text-GrayishBlue ">
+          {" "}
+          Our Artificial Intelligence powered tools use millions of project data
+          points to ensure that your project is successful
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        {/* the grid container */}
+        <div className="grid"></div>
+        {/*  */}
+      </div>
+    </main>
   );
 }
 
